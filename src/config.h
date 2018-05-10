c86e977b8aca288d2a692c29de3073b7b5b07721
zliu1022 <zliu1022@gmail.com>
2018-05-10 17:06:01 +0800

    Update 0.15
    主要和生成自对弈有关：
    Bugfix to Dirichlet noise being more uniform than intended (bug was introduced in v0.13).
    Tweaks to randomized move selection in training games to reduce needless blunders. Added extra options.

--- src/config.h
+++ src/config.h
@@ -31,10 +31,11 @@
 #endif
 
 /*
- * BOARD_SIZE: Define size of the board to compile Leela with, must be an odd number due to winograd tiles
+ * BOARD_SIZE: Define size of the board to compile Leela with, must be an odd
+   number due to winograd tiles
  */
 #define BOARD_SIZE 19
-#define BOARD_SQUARES BOARD_SIZE*BOARD_SIZE
+#define BOARD_SQUARES (BOARD_SIZE*BOARD_SIZE)
 
 #if (BOARD_SIZE % 2 == 0)
 #error Code assumes odd board size, remove at your own risk!
@@ -77,10 +78,10 @@
  * USE_TUNER: Expose some extra command line parameters that allow tuning the
  * search algorithm.
  */
-//#define USE_TUNER
+#define USE_TUNER
 
 #define PROGRAM_NAME "Leela Zero"
-#define PROGRAM_VERSION "0.14"
+#define PROGRAM_VERSION "0.15"
 
 /*
  * OpenBLAS limitation: the default configuration on some Linuxes
