c0f312806e7fe19402bb4a02807230429c7a71c4
zliu1022 <zliu1022@gmail.com>
2019-07-30 11:42:41 +0800

    optimize pacman
    对于pass的优化，可能出现无棋可走的情况，比如只剩对方的眼，自己的眼是自杀，所以只能pass
    因此不能把uct_select修改成完全不选pass，只能尽量压低

--- src/UCTNode.cpp
+++ src/UCTNode.cpp
@@ -308,6 +308,9 @@ UCTNode* UCTNode::uct_select_child(int color, bool is_root) {
         auto denom = 1.0 + child->get_visits();
         auto puct = cfg_puct * psa * (numerator / denom);
         auto value = winrate + puct;
+        if (cfg_pacman&&(child->get_move()==FastBoard::PASS)) {
+            value = 0;
+        }
         assert(value > std::numeric_limits<double>::lowest());
 
         if (value > best_value) {
